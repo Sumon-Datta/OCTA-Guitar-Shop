0d200fc2132fa269392eff51100eaedc324f8764
Sumon-Datta <sumondatta16@gmail.com>
2022-04-05 20:51:51 +0600

    update setup

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-//import logo from "./logo.svg";
-import "./App.css";
-import HomePage from "./Component/HomePage/HomePage";
-import Dashboard from "./Component/Dashboard/Dashboard";
-import Blogs from "./Component/Blogs/Blogs";
-import About from "./Component/About/About";
-import Header from './Component/Header/Header';
-import Reviewer from './Component/Reviewer/Reviewer';
-import { Routes, Route } from "react-router-dom";
-import NotFound from './Component/NotFound/NotFound';
-import Footer from "./Component/Footer/Footer";
-import initFontAwesome from './Component/FontAwesome/FontAwesome';
-initFontAwesome();
+// //import logo from "./logo.svg";
+// import "./App.css";
+// import HomePage from "./Component/HomePage/HomePage";
+// import Dashboard from "./Component/Dashboard/Dashboard";
+// import Blogs from "./Component/Blogs/Blogs";
+// import About from "./Component/About/About";
+// import Header from './Component/Header/Header';
+// import Reviewer from './Component/Reviewer/Reviewer';
+// import { Routes, Route } from "react-router-dom";
+// import NotFound from './Component/NotFound/NotFound';
+// import Footer from "./Component/Footer/Footer";
+// import initFontAwesome from './Component/FontAwesome/FontAwesome';
+// initFontAwesome();
 
 
-function App() {
-  return (
-     <div>
-       <div>
-       <Header></Header>
-     </div>
-      <div>
-        <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/reviewers" element={<Reviewer />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      </div>
-      <div>
-        <Footer></Footer>
-      </div>
-     </div>
+// function App() {
+//   return (
+//      <div>
+//        <div>
+//        <Header></Header>
+//      </div>
+//       <div>
+//         <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//         <Route path="/reviewers" element={<Reviewer />} />
+//         <Route path="/blogs" element={<Blogs />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//       </div>
+//       <div>
+//         <Footer></Footer>
+//       </div>
+//      </div>
   
-  );
-}
+//   );
+// }
 
-export default App;
+// export default App;
